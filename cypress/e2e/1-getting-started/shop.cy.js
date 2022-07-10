ccfc9e2535389e51446a85aef33ace236efe408b
SquadTeemo <patrice_office@yahoo.com>
2022-07-10 16:28:01 +0100

    Cypress E2E tests finished

--- cypress/e2e/1-getting-started/shop.cy.js
+++ cypress/e2e/1-getting-started/shop.cy.js
@@ -1,12 +1,61 @@
-describe("Shop app", function () {
-  it("front page can be opened", function () {
+describe("Main pages can be opened", function () {
+  it("Home page can be opened", function () {
     cy.visit("http://localhost:3000");
     cy.contains("Hygiene");
   });
-});
-describe("Shop app", function () {
   it("front page can be opened", function () {
     cy.visit("http://localhost:3000/shop");
     cy.contains("Today");
+    cy.contains("ZEWA");
+    cy.contains("TEMPO");
+  });
+  it("front page can be opened", function () {
+    cy.visit("http://localhost:3000/basket");
+    cy.contains("Your basket content");
+    cy.contains("Total to pay:");
+  });
+});
+
+describe("Navigation is working", function () {
+  it("navigation click test", function () {
+    cy.visit("http://localhost:3000");
+    cy.contains("SHOP").click();
+    cy.contains("ZEWA");
+    cy.contains("TEMPO");
+    cy.visit("http://localhost:3000");
+    cy.contains("BASKET").click();
+    cy.contains("Your basket content");
+    cy.contains("Total to pay:");
+  });
+});
+
+describe("Shopping", function () {
+  it("navigation click test", function () {
+    cy.visit("http://localhost:3000");
+    cy.contains("SHOP").click();
+    cy.contains("ZEWA Toilet Paper").click();
+    cy.contains("+").click();
+    cy.contains("+").click();
+    cy.contains("+").click();
+    cy.contains("+").click();
+    cy.contains("+").click();
+    cy.contains("+").click();
+    cy.contains("+").click();
+    cy.contains("Add to cart").click();
+    cy.contains("Back to selection").click();
+    cy.contains("TEMPO Face Mask").click();
+    cy.contains("+").click();
+    cy.contains("+").click();
+    cy.contains("Add to cart").click();
+    cy.contains("To the Basket").click();
+    cy.contains("Your basket content:").click();
+    cy.contains("7 ZEWA Toilet Paper");
+    cy.contains("£4.55");
+    cy.contains("£0.65");
+    cy.contains("2 TEMPO Face Mask");
+    cy.contains("£5.00");
+    cy.contains("£1.00");
+    cy.contains("Total to pay:");
+    cy.contains("£7.90");
   });
 });
